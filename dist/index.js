53192ce6fed2d706bf251c45a89ab9ef3907fc2b
Ankit Suvarna <ankitsuvarna11@gmail.com>
2017-11-17 20:16:44 +0530

    testing 7

--- dist/index.js
+++ dist/index.js
@@ -29,17 +29,56 @@ app.use(_bodyParser2.default.json());
 // adding logger
 app.use((0, _morgan2.default)("dev"));
 
-// Index route
-app.get('/', function (req, res) {
-  return res.send('Hello world, I am a chat bot');
+//create endpoint for webhook
+app.post('/webhook', function (req, res) {
+
+  var body = req.body;
+
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function (entry) {
+
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
+      var webhookEvent = entry.messaging[0];
+      console.log(webhookEvent);
+    });
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
 });
 
-// for Facebook verification
-app.get('/webhook/', function (req, res) {
-  if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-    res.send(req.query['hub.challenge']);
+// Facebook verification
+app.get('/webhook', function (req, res) {
+
+  // Your verify token. Should be a random string.
+  var VERIFY_TOKEN = "thisissecrettoken";
+
+  // Parse the query params
+  var mode = req.query['hub.mode'];
+  var token = req.query['hub.verify_token'];
+  var challenge = req.query['hub.challenge'];
+
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
   }
-  res.send('Error, wrong token');
 });
 
 // Listen to the server
