78ffaa46b451acdfa4602a5660326bfb4d2802d9
Ankit Suvarna <ankitsuvarna11@gmail.com>
2017-11-17 20:09:37 +0530

    testing 5

--- index.js
+++ index.js
@@ -14,16 +14,63 @@ app.use(bodyParser.json());
 // adding logger
 app.use(morgan("dev"));
 
+
+//create endpoint for webhook
+app.post('/webhook', (req, res) => {  
+  
+  let body = req.body;
+  
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+    
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+      
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
+      let webhookEvent = entry.messaging[0];
+      console.log(webhookEvent);
+    });
+    
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+  
+});
+
+// Facebook verification
+app.get('/webhook', (req, res) => {
+  
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
+  
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+  
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+    
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+      
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
+    }
+  }
+});
+
 // Index route
 app.get('/', (req, res) => res.send('Hello world, I am a chat bot'));
-
-// for Facebook verification
-app.get('/webhook/', (req, res) => {
-  if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-    res.send(req.query['hub.challenge']);
-  }
-  res.send('Error, wrong token');
-});
 
 // Listen to the server
 app.listen(app.get('port'), () => console.log('running on port', app.get('port')));
